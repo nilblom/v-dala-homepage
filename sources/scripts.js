9080d2f98af94573eb017b7ab21fd47e53bf8f2f
Nils Blomqvist <nblomqvist@gmail.com>
2025-04-21 15:12:56 +0200

    Implement contact helper on contact page.

--- sources/scripts.js
+++ sources/scripts.js
@@ -1,10 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Your code here
     enable_translate();
 
     if (localStorage.getItem("language") == "en")
     	translate();
+
+    enable_contact_helper();
 });
+
+
+/* Functions to enable translation to English. */
 
 function enable_translate() {
 	document.getElementById("switch-language").addEventListener("click", function(e) {
@@ -22,4 +26,14 @@ function translate() {
 		node.innerText = node.getAttribute("t");
 		node.setAttribute("t", original_text);
 	});
+}
+
+function enable_contact_helper() {
+	var helper = document.getElementById("contact-helper-select");
+	var display = document.getElementById("contact-helper-display");
+	helper.onchange = function() {
+		display.value = helper.value;
+	}
+	// Initialize the helper.
+	display.value = helper.value;
 }
