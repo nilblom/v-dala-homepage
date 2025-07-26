8b5f74f213405e3f8d31626c428e840a614e9513
Nils Blomqvist <nblomqvist834@gmail.com>
2025-07-26 13:27:54 +0200

    (Mobile) Add link to History page.

--- generate_website.py
+++ generate_website.py
@@ -124,7 +124,8 @@ mixed_files_mobile = [
 mobile_pages = [
 	(r"sources\mobile", "index.html.page"),
 	(r"sources\mobile", "accommodation.html.page"),
-	(r"sources\mobile", "event.html.page")
+	(r"sources\mobile", "event.html.page"),
+	(r"sources\mobile", "history.html.page")
 ]
 
 output_folder = "upload"
